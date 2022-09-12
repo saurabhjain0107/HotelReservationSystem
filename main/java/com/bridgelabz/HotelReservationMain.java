7696bf77bac650cc08270712d931e1efc8f34561
saurabh jain <luckyjain0107@gmai.com>
2022-09-12 18:43:14 +0530

    Welcome message

---
+++ main/java/com/bridgelabz/HotelReservationMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class HotelReservationMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System:");
+    }
+}
